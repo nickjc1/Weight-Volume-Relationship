8fdcb749a44d1825a2a79269f14611ba03bc8d8a
CHAO JIANG <cjiang@CHAOs-MacBook-Pro.local>
2018-06-07 17:03:39 -0500

    add pickerview for picking the value of unit weight of water

--- WeightVolume/Controller/ViewController.swift
+++ WeightVolume/Controller/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     //declare variable here:
     @IBOutlet weak var stackView: UIStackView!
     
+    @IBOutlet weak var unitWeightWaterPicker: UIPickerView!
+    let unitWeightWater = [9.8, 62.4]
+    
     @IBOutlet var messageTextFields: [UITextField]!
     
     override func viewDidLoad() {
@@ -23,9 +26,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
             tf.delegate = self
         }
         
-        //set gesture:
+        //set yourself as delegate of pickerview
+        unitWeightWaterPicker.delegate = self
+        unitWeightWaterPicker.dataSource = self
+        
+        //set gesture for stackview so that we can handle keyboard hidding problem:
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(stackViewTapped))
         stackView.addGestureRecognizer(tapGesture)
+        
+        //default unit weight water data and default picker is hidden:
+        messageTextFields[3].text = String(unitWeightWater[0])
+        unitWeightWaterPicker.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,6 +51,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
         }
     }
+    
+    
+    
+    // MARK: - submit and reset button pressed:
 
     @IBAction func infoButtonPressed(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: "goToInfo", sender: self)
@@ -51,5 +66,35 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+}
+
+// MARK: - picker view delegate and datasource method:
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    //set number of components in picker view:
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    //set number of rows in each components:
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return unitWeightWater.count
+    }
+    //set each title of each rows to be displayed:
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(unitWeightWater[row])
+    }
+    //row is selected:
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        messageTextFields[3].text = String(unitWeightWater[row])
+        unitWeightWaterPicker.isHidden = true
+    }
+    //hiding picker view when not using:
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if textField == messageTextFields[3] {
+            unitWeightWaterPicker.isHidden = false
+            return false
+        }
+        return true
+    }
 }
 
